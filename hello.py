b8927469672940857f809b02fb7410e4cf1cc0f2
Anoop <anoop@byond.travel>
2015-07-15 17:42:12 +0530

    comment added in hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,4 @@ print "hello world"
 # hi changes made in dev code..
 print 2+3
 print 2*3
-# just this would do ..
-# this didn't work
-# at least this :(
-# how depressing is this ?
+# adds and multiplies number
