346cfb86ce7c940818eb0d681538930ad0ee5833
Anoop <anoop@byond.travel>
2015-07-16 15:02:07 +0530

    hello updated

--- hello.py
+++ hello.py
@@ -1,7 +1,4 @@
 print "hello world"
 # hi changes made in dev code..
 print 2+3
-print 2*3
-# adds and multiplies number
-# last commit to master didn't work
-# adding comment on Jul 16 
+print 2*16 
